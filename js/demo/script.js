d2f0b85deeafb0ec69142bbf068da0307db8c7e3
Wolfyxon <wolfyxon@gmail.com>
2024-08-04 20:59:59 +0200

    Added file encryption and decryption

--- js/demo/script.js
+++ js/demo/script.js
@@ -1,4 +1,7 @@
 window.addEventListener("load", () => {
+    const textEncoder = new TextEncoder();
+    const textDecoder = new TextDecoder();
+
     /*
     const text = {
         btnEncrypt: document.getElementById("btn-encrypt-text"),
@@ -7,8 +10,6 @@ window.addEventListener("load", () => {
         password:   document.getElementById("text-password")
     };
 
-    const textEncoder = new TextEncoder();
-    const textDecoder = new TextDecoder();
 
     text.btnEncrypt.addEventListener("click", () => {
         const value = textEncoder.encode(text.input.value);
@@ -27,11 +28,48 @@ window.addEventListener("load", () => {
         text.input.value = textDecoder.decode(decrypted);
     });*/
 
+    const fileReader = new FileReader();
+
     const file = {
         btnEncrypt: document.getElementById("btn-encrypt-file"),
         btnDecrypt: document.getElementById("btn-decrypt-file"),
         input:      document.getElementById("file-input"),
         password:   document.getElementById("file-password")
     };
+
+    function downloadBuffer(buffer, fileName) {
+        const blob = new Blob([buffer]);
+        
+        const a = document.createElement("a");
+        a.href = URL.createObjectURL(blob);
+        a.download = fileName;
+        a.click();
+    }
+
+    file.btnEncrypt.addEventListener("click", () => {
+        const f = file.input.files[0];
+        if(!f) return;
+
+        fileReader.readAsArrayBuffer(f);
+
+        fileReader.onload = () => {
+            const data = new Uint8Array(fileReader.result);
+            const encrypted = benc.encrypt(data, textEncoder.encode(file.password.value));
+            downloadBuffer(encrypted, f.name);
+        }
+    });
+
+    file.btnDecrypt.addEventListener("click", () => {
+        const f = file.input.files[0];
+        if(!f) return;
+
+        fileReader.readAsArrayBuffer(f);
+
+        fileReader.onload = () => {
+            const data = new Uint8Array(fileReader.result);
+            const decrypted = benc.decrypt(data, textEncoder.encode(file.password.value));
+            downloadBuffer(decrypted, f.name);
+        }
+    });
 
 });
