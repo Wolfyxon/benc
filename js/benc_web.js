20eae7ea641a40628a55d0e0beb10cd94d86a133
Wolfyxon <wolfyxon@gmail.com>
2024-08-04 12:02:43 +0200

    Using Unit8Arrays

--- js/benc_web.js
+++ js/benc_web.js
@@ -5,18 +5,18 @@ const benc = {};
 
 /**
  * Encrypts bytes
- * @param {number[]} data 
- * @param {number[]} key
- * @returns {number[]}
+ * @param {Uint8Array} data 
+ * @param {Uint8Array} key
+ * @returns {Uint8Array}
  */
 benc.encrypt = function(data, key) {
-    const res = [];
+    const res = new Uint8Array(data,length);
 
     for(let i = 0; i < data.length; i++) {
         const byte = data[i];
         const keyByte = key[i % key.length];
 
-        res.push(byte + keyByte);
+        res[i] = byte + keyByte;
     }
 
     return res;
@@ -24,18 +24,18 @@ benc.encrypt = function(data, key) {
 
 /**
  * Decrypts bytes
- * @param {number[]} encrypted 
- * @param {number[]} key
- * @returns {number[]}
+ * @param {Uint8Array} encrypted 
+ * @param {Uint8Array} key
+ * @returns {Uint8Array}
  */
 benc.decrypt = function(encrypted, key) {
-    const res = [];
+    const res = new Uint8Array(encrypted.length);
 
     for(let i = 0; i < encrypted.length; i++) {
         const byte = encrypted[i];
         const keyByte = key[i % key.length];
 
-        res.push(byte - keyByte);
+        res[i] = byte - keyByte;
     }
 
     return res;
